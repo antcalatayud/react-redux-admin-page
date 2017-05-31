54f81b5142722bbbb9342948adb25f7ea6e05c03
Antonio Calatayud <Antonio.Calatayud@terraquest.co.uk>
2017-05-31 08:28:17 +0100

    Add courses to the app

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import Home from './components/Home/Home';
 import About from './components/About/About';
+import Courses from './components/Course/Courses';
 
 export default (
     <Route path="/" component={App}>
-        <IndexRoute component={Home}/>
+        <IndexRoute component={Home}/>        
+        <Route path="courses" component={Courses}/>
         <Route path="about" component={About}/>
     </Route>
 );
