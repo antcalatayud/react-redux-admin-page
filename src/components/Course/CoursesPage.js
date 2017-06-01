8684ecf6417f14bf86a999e7d2b50948cd3d4c73
Antonio Calatayud <Antonio.Calatayud@terraquest.co.uk>
2017-06-01 12:14:36 +0100

    Move list of courses to different component. Add course will be added

--- src/components/Course/CoursesPage.js
+++ src/components/Course/CoursesPage.js
@@ -1,56 +1,28 @@
-import React from 'react';
+import React, { PropTypes} from 'react';
 import { connect } from 'react-redux';
 import * as courseActions from '../../actions/CourseActions';
 import {bindActionCreators} from'redux';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
     constructor(props, context){
         super(props,context);
-        this.state = {
-            course:{
-                title: ""
-            }
-        };
-        this.onTitleChange = this.onTitleChange.bind(this);
-        this.onClickSave = this.onClickSave.bind(this);
-    }
-
-    onTitleChange(event){
-        const course = this.state.course;
-        course.title = event.target.value;
-        this.setState({course:course});
-    }
-
-    onClickSave(event){
-        this.props.actions.createCourse(this.state.course);
-    }
-
-    courseRow(course, index){
-        return <div key={index}> {course.title}</div>;
     }
 
     render() {
+        const { courses } = this.props;
         return (
             <div>
                 <h1>Courses</h1>
-                {this.props.courses.map(this.courseRow)}
-                <h2>Add Course</h2>
-                <input 
-                    type="text"
-                    onChange={this.onTitleChange}
-                    value={this.state.course.title} />
-                <input 
-                    type="submit"
-                    value="Save"
-                    onClick={this.onClickSave}/>
+                <CourseList courses={courses} />               
             </div>            
         );
     }
 }
 
 CoursesPage.propTypes = {
-    courses: React.PropTypes.array.isRequired,
-    actions: React.PropTypes.object.isRequired
+    courses: PropTypes.array.isRequired,
+    actions: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
