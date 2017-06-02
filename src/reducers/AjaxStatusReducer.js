67fdf286c280142e6fd2c6aeaee04bc6ece367db
Antonio Calatayud <Antonio.Calatayud@terraquest.co.uk>
2017-06-02 10:28:42 +0100

    Create Ajax status reducer

---
+++ src/reducers/AjaxStatusReducer.js
@@ -0,0 +1,16 @@
+import * as types from '../actions/actionTypes';
+import InitialState from './InitialState';
+
+function actionTypeEndsInSuccess(type) {
+    return type.substring(type.length - 8) == '_SUCCESS';    
+}
+
+export default (state = InitialState.numAjaxCallsInProgress, action) => {
+    if (action.type == types.BEGIN_AJAX_CALL) {
+        return state + 1;
+    } 
+    else if (actionTypeEndsInSuccess(action.type)){
+        return state - 1;
+    }
+    return state;
+};
