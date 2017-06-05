8d898f9e92f42d1198556dc4e42c7c10c4770d5b
Antonio Calatayud <Antonio.Calatayud@terraquest.co.uk>
2017-06-05 16:36:03 +0100

    Add tests. added Test Utils and Enzime to check differences

---
+++ src/components/Course/CourseForm.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import expect from 'expect';
+import { mount, shallow } from 'enzyme'; 
+import CourseForm from './CourseForm';
+
+function setup(saving) {
+    let props = {
+        course: {},
+        saving: saving,
+        allAuthors: {},
+        errors: {},
+        onSave: () => {},
+        onChange:() => {}
+    };
+
+    return shallow (
+        <CourseForm {...props}/> 
+    );
+}
+
+
+describe('CourseForm via Enzyme', () => {
+    it('should render Form and <h1></h1>', () => {
+        const wrapper = setup();
+        expect(wrapper.find('form').length).toBe(1);
+        expect(wrapper.find('h1').text()).toEqual('Manage Course');
+    });
+
+    it('should save button is labeled "Save" when not saving', () => {
+        const wrapper = setup(false);
+        expect(wrapper.find('input').props().value).toBe('Save');
+    });
+
+    it('should save button is labeled "Saving..." when saving', () => {
+        const wrapper = setup(true); 
+        expect(wrapper.find('input').props().value).toBe('Saving...');
+    });
+});
