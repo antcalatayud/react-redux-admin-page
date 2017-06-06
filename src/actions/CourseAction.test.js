fa2c61ddde12f63a5b5166500225402c4cd8eef7
Antonio Calatayud <Antonio.Calatayud@terraquest.co.uk>
2017-06-06 09:46:25 +0100

    Test action finish

--- src/actions/CourseAction.test.js
+++ src/actions/CourseAction.test.js
@@ -6,12 +6,15 @@ describe('Course Actions', () => {
     describe('createCourseSucces', () => {
         it('should create a CREATE_COUSE_SUCCESS action', () => {
             //arrange
-
+            const course = {id: "new-course", title: "New Course"};
+            const expectedAction = {
+                type: types.CREATE_COURSE_SUCCESS,
+                course: course
+            };
             //act
-
+            const action = courseActions.createCourseSuccess(course);
             //assert
-            
-        });
-        
+            expect(action).toEqual(expectedAction);
+        });        
     });
 });
