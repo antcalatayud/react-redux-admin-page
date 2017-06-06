1384bddf6f774753fc34df05e292b75735b7788c
Antonio Calatayud <Antonio.Calatayud@terraquest.co.uk>
2017-06-06 09:43:14 +0100

    Test actions. Create structure

---
+++ src/actions/CourseAction.test.js
@@ -0,0 +1,17 @@
+import expect from 'expect';
+import * as types from './actionTypes';
+import * as courseActions from './CourseActions';
+
+describe('Course Actions', () => {
+    describe('createCourseSucces', () => {
+        it('should create a CREATE_COUSE_SUCCESS action', () => {
+            //arrange
+
+            //act
+
+            //assert
+            
+        });
+        
+    });
+});
